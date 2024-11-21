22abce266baa693c66f140292e7103d813c0bc6f
kimgh06 <kkh061101@naver.com>
2024-11-21 13:29:19 +0900

    8방향 이동

--- src/components/arrowkeys/index.tsx
+++ src/components/arrowkeys/index.tsx
@@ -3,15 +3,31 @@ import useCursorStore from '@/store/cursorStore';
 import S from './style.module.scss';
 
 export default function ArrowKeys() {
-  const { x, y, setY, setX } = useCursorStore();
+  const { x, y, setY, setX, setPosition } = useCursorStore();
   const [activedButton, setActivedButton] = useState('');
+  const upLeft = () => {
+    setActivedButton('upLeft');
+    setPosition(x - 1, y - 1);
+  };
   const up = () => {
     setActivedButton('up');
     setY(y - 1);
   };
+  const upRight = () => {
+    setActivedButton('upRight');
+    setPosition(x + 1, y - 1);
+  };
+  const downLeft = () => {
+    setActivedButton('downLeft');
+    setPosition(x - 1, y + 1);
+  };
   const down = () => {
     setActivedButton('down');
     setY(y + 1);
+  };
+  const downRight = () => {
+    setActivedButton('downRight');
+    setPosition(x + 1, y + 1);
   };
   const left = () => {
     setActivedButton('left');
@@ -22,57 +38,37 @@ export default function ArrowKeys() {
     setX(x + 1);
   };
 
-  useEffect(() => {
-    if (!window) {
-      return;
-    }
-
-    const handleKeyDown = (e: KeyboardEvent) => {
-      if (activedButton !== '') return;
-      switch (e.key) {
-        case 'ArrowUp':
-          up();
-          break;
-        case 'ArrowDown':
-          down();
-          break;
-        case 'ArrowLeft':
-          left();
-          break;
-        case 'ArrowRight':
-          right();
-          break;
-        default:
-          break;
-      }
-      setActivedButton('');
-    };
-
-    // Adding event listener
-    window.addEventListener('keydown', handleKeyDown);
-
-    // Cleanup the event listener
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [x, y]);
-
   return (
     <div className={S.buttons} onPointerUp={() => setActivedButton('')}>
       <div className={S.row}>
-        <button onPointerDown={up} className={`${activedButton === 'up' && S.active}`}>
-          <div className={S.text}>Up</div>
+        <button onPointerDown={upLeft} className={`${activedButton === 'upLeft' && S.active}`}>
+          <div className={S.text}>↖</div>
         </button>
-        <button onPointerDown={right} className={`${activedButton === 'right' && S.active}`}>
-          <div className={S.text}>Right</div>
+        <button onPointerDown={up} className={`${activedButton === 'up' && S.active}`}>
+          <div className={S.text}>↑</div>
+        </button>
+        <button onPointerDown={upRight} className={`${activedButton === 'right' && S.active}`}>
+          <div className={S.text}>↗</div>
         </button>
       </div>
       <div className={S.row}>
         <button onPointerDown={left} className={`${activedButton === 'left' && S.active}`}>
-          <div className={S.text}>Left</div>
+          <div className={S.text}>←</div>
+        </button>
+        <button>○</button>
+        <button onPointerDown={right} className={`${activedButton === 'right' && S.active}`}>
+          <div className={S.text}>→</div>
+        </button>
+      </div>
+      <div className={S.row}>
+        <button onPointerDown={downLeft} className={`${activedButton === 'downLeft' && S.active}`}>
+          <div className={S.text}>↙</div>
         </button>
         <button onPointerDown={down} className={`${activedButton === 'down' && S.active}`}>
-          <div className={S.text}>Down</div>
+          <div className={S.text}>↓</div>
+        </button>
+        <button onPointerDown={downRight} className={`${activedButton === 'downRight' && S.active}`}>
+          <div className={S.text}>↘</div>
         </button>
       </div>
     </div>
