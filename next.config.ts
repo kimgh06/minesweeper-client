a95b7d384b86f0c5f0f8fdc05e518d08287a202f
kimgh06 <kkh061101@naver.com>
2024-12-05 16:30:44 +0900

    slash problem

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
   },
   output: 'export',
   basePath: '/minesweeper-client', // GitHub Pages에서 사용하는 경로로 설정
-  assetPrefix: '/minesweeper-client/', // GitHub Pages에서 사용하는 경로로 설정
+  assetPrefix: '/minesweeper-client', // GitHub Pages에서 사용하는 경로로 설정
   trailingSlash: true, // 슬래시로 끝나게 설정
 };
 
