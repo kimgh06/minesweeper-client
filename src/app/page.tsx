950702c7ea25b5e6e0e4539b922674415d7850e4
kimgh06 <kkh061101@naver.com>
2024-12-17 12:27:03 +0900

    update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 import Link from 'next/link';
+import S from './style.module.scss';
 
 export default function Home() {
-  return <Link href="/play">Play</Link>;
+  return (
+    <div className={S.page}>
+      <div className={S.welcome}>
+        <h2>Welcome to</h2>
+        <h1>Gamulpung</h1>
+        <Link href="/play">
+          <button>Play</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
