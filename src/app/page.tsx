a64120c8b53f789fa5c493bbdda888fa22d5e1df
kimgh06 <kkh061101@naver.com>
2024-12-19 13:45:01 +0900

    landing templete complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import S from './style.module.scss';
+import StepVideo from '@/components/stepVideo';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -13,6 +15,17 @@ export default function Home() {
       </div>
       <div className={S.rules}>
         <h1>How to Play</h1>
+        <StepVideo num={1} text="Download the game" source="/gamulpung-client/step1.mp4" />
+        <StepVideo num={1} text="Download the game" source="/gamulpung-client/step1.mp4" />
+      </div>
+      <div className={S.contribute}>
+        <div className={S.mainBlock}>
+          <h1>If you want to Contribute</h1>
+          <Link href="/documents/contribute-guide">
+            <button>Contribute</button>
+          </Link>
+        </div>
+        <Image src="/gamulpung-client/landingTile.svg" alt="tile" width={400} height={800} />
       </div>
     </div>
   );
---
+++ src/components/stepVideo/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import S from './style.module.scss';
+
+export default function StepVideo({ num, text, source: gif }: { num: number; text: string; source: string }) {
+  return (
+    <div className={S.stepVideo}>
+      <div>
+        <p>Step {num}</p>
+        <p>{text}</p>
+      </div>
+      <Image src={gif} alt={text} width={400} height={300} />
+    </div>
+  );
+}
